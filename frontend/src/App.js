d99c0380cb08a5bdb38d8dac87b5ceed97216511
rsrinidhii <srinidhiranga20@gmail.com>
2025-05-08 22:30:23 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PredictPage from "./components/PredictPage";
+
+function App() {
+  return (
+    <div className="App">
+      <PredictPage />
+    </div>
+  );
+}
+
+export default App;
